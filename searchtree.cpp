5be4db8061e8195e9e63cc2b5da6418c48f9db06
amin240203 <aminfebryansyah240203@gmail.com>
2026-06-09 10:46:45 +0700

    constructor for node class

--- searchtree.cpp
+++ searchtree.cpp
@@ -2,5 +2,15 @@
 #include <string>
 using namespace std;
 
-class Node {
+class node {
+    public:
+    string info;
+        node *leftchild;
+        node *rightchild;
+        node(string i, node *l, node *r)
+        {
+            info = i;
+            leftchild = l;
+            rightchild = r;
+        }
 }
